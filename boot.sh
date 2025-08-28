4a2930f2e6fb8ce3c40c75e3b6415a0d8c6d2770
Mattias De Coninck <59693063+mattias-de-coninck@users.noreply.github.com>
2025-08-28 13:53:46 +0200

    fix(boot.sh): username typo

--- boot.sh
+++ boot.sh
@@ -15,7 +15,7 @@ echo -e "\n$ansi_art\n"
 sudo pacman -Sy --noconfirm --needed git
 
 # Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-mattias-de-connick/omarchiri}"
+OMARCHY_REPO="${OMARCHY_REPO:-mattias-de-coninck/omarchiri}"
 
 echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
